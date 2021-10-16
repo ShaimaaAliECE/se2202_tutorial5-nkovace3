a019bd543f08f76c5cb9a31685cc7a6934bed84e
Nikola <nkovace3@uwo.ca>
2021-10-16 00:43:35 -0400

    Tutorial 5

--- scripts.js
+++ scripts.js
@@ -3,13 +3,21 @@ let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the gam
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
-
+let p1=document.querySelector("#next-lbl");
+p1.innerHTML=nextPlayer;
 
 //This call will create the buttons needed for the gameboard.
-createGameBoard()
+createGameBoard();
 
 function createGameBoard()
 {
+    let squares=document.querySelectorAll("td");
+    let brackets=`<button id='edit-btn'>[ ]</button>`;
+
+    for(let i=0;i<squares.length;i++){
+        squares[i].innerHTML=brackets;
+    }
+    
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
    
 }
@@ -24,16 +32,35 @@ for (let i=0; i<btns.length; i++)
 // This function will be used to respond to a click event on any of the board buttons.
 function takeCell(event)
 {
+    
+    let buttonClick=event.target;
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
+    if(buttonClick.innerHTML==="[ ]"){
+        if(nextPlayer==='X'){
+            buttonClick.innerHTML="["+ nextPlayer +"]";
+            nextPlayer="O";
+            buttonClick.disabled=true;
+            p1.innerHTML=nextPlayer;
+        }
 
+        else if(nextPlayer==='O'){
+            buttonClick.innerText="["+nextPlayer+"]";
+            nextPlayer="X";
+            buttonClick.disabled=true;
+            p1.innerHTML=nextPlayer;
+        }
+
+    }
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
 
     // Check if the game is over
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        let gameOverSign=document.querySelector("#game-over-lbl");
+        gameOverSign.innerHTML=`<h1>Game Over</h1>`;
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -43,4 +70,17 @@ function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
    
+    let disBtnNum=0;
+
+    for(i in btns){
+        if(btns[i].disabled===true){
+            disBtnNum++;
+        }
+    }
+    if(disBtnNum==9){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
